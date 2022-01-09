cee2fcae1bb1a28f3adc6f0da54e745778750ce9
Adilson Chameia <adilsonkchameia@outlook.com>
2022-01-09 21:38:47 +0100

    otimizacao de navegacao

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/pages/avatar_page.dart';
 import 'package:componentes/src/pages/home_page.dart';
+import 'package:componentes/src/routes/routes.dart';
 //import 'package:componentes/src/pages/home_temp.dart';
 import 'package:flutter/material.dart';
 
@@ -10,15 +11,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      debugShowCheckedModeBanner: false,
-      //home: HomePage());
-      initialRoute: '/',
-      routes: <String, WidgetBuilder>{
-        '/': (BuildContext context) => HomePage(),
-        'alert': (BuildContext context) => AlertPage(),
-        'avatar': (BuildContext context) => AvatarPage(),
-      },
-    );
+        title: 'Material App',
+        debugShowCheckedModeBanner: false,
+        //home: HomePage());
+        initialRoute: '/',
+        routes: getApplicationRoutes(),
+        //Quando nao tem uma pagina
+        onGenerateRoute: (settings) {
+          return MaterialPageRoute(
+              builder: (BuildContext context) => AlertPage());
+        });
   }
 }
---
+++ lib/src/routes/routes.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:componentes/src/pages/alert_page.dart';
+import 'package:componentes/src/pages/avatar_page.dart';
+import 'package:componentes/src/pages/home_page.dart';
+
+//Chamando Metodo Que Tem As Rotas
+Map<String, WidgetBuilder> getApplicationRoutes() {
+  return <String, WidgetBuilder>{
+    '/': (BuildContext context) => HomePage(),
+    'alert': (BuildContext context) => AlertPage(),
+    'avatar': (BuildContext context) => AvatarPage()
+  };
+}
