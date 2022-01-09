16b0441d436a4d3784fbf01a599aa36c9d74bfaf
Adilson Chameia <adilsonkchameia@outlook.com>
2022-01-09 22:49:58 +0100

    card personalizado com load de um gif

---
+++ lib/src/pages/card_page.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class CardPage extends StatelessWidget {
+  const CardPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Card Page'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(10.0),
+        children: [_cardTipo1(), SizedBox(height: 30.0), _cardTipo2()],
+      ),
+    );
+  }
+
+  Widget _cardTipo1() {
+    return Card(
+      child: Column(
+        children: [
+          ListTile(
+            leading: Icon(
+              Icons.photo_album,
+              color: Colors.blue,
+            ),
+            title: Text('Titulo desse Card'),
+            subtitle: Text(
+                'Ca vai o exemplo de um texto longo do card para ver como ele fica realmente.'),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              TextButton(
+                onPressed: () {},
+                child: Text('Cancelar'),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Text('Ok'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _cardTipo2() {
+    return Card(
+      child: Column(
+        children: [
+          FadeInImage(
+            placeholder: AssetImage('assets/jar-loading.gif'),
+            image: NetworkImage(
+                'https://i.natgeofe.com/n/2a832501-483e-422f-985c-0e93757b7d84/6.jpg'),
+          ),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: Text('Imagem'),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,3 +1,4 @@
+import 'package:componentes/src/pages/card_page.dart';
 import 'package:flutter/material.dart';
 import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/pages/avatar_page.dart';
@@ -8,6 +9,7 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
   return <String, WidgetBuilder>{
     '/': (BuildContext context) => HomePage(),
     'alert': (BuildContext context) => AlertPage(),
-    'avatar': (BuildContext context) => AvatarPage()
+    'avatar': (BuildContext context) => AvatarPage(),
+    'card': (BuildContext context) => CardPage()
   };
 }
