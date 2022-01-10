49b149fbd40c1b342af989c2654589df9e4e07e1
Adilson Chameia <adilsonkchameia@outlook.com>
2022-01-10 08:50:50 +0100

    final do primeiro alerta

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -9,6 +9,13 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alert Page'),
       ),
+      body: Center(
+          child: RaisedButton(
+        onPressed: () => _mostrarAlert(context),
+        child: Text('Mostrar Alerta'),
+        textColor: Colors.blue,
+        shape: StadiumBorder(),
+      )),
       floatingActionButton: FloatingActionButton(
           child: Icon(Icons.close),
           onPressed: () {
@@ -16,4 +23,38 @@ class AlertPage extends StatelessWidget {
           }),
     );
   }
+}
+
+void _mostrarAlert(BuildContext context) {
+  showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          title: Text('Titulo'),
+          content: Column(
+            //Se adapte ao conteudo
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('Este conteudo e da caixa da alerta.'),
+              FlutterLogo(
+                size: 100.0,
+              ),
+            ],
+          ),
+          actions: [
+            FlatButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text('Cancelar'),
+            ),
+            FlatButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: Text('Ok'),
+            )
+          ],
+        );
+      });
 }
